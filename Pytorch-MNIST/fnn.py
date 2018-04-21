a83320c4142c62e9a8d5b555585240dc3ac45db9
Deren Lei <derenlei@umail.ucsb.edu>
2018-04-21 11:33:43 -0700

    fnn support cpu

--- Pytorch-MNIST/fnn.py
+++ Pytorch-MNIST/fnn.py
@@ -8,13 +8,13 @@ from torch.autograd import Variable
 STEP 1: LOADING DATASET
 '''
 
-train_dataset = dsets.MNIST(root='./data', 
-                            train=True, 
+train_dataset = dsets.MNIST(root='./data',
+                            train=True,
                             transform=transforms.ToTensor(),
                             download=True)
 
-test_dataset = dsets.MNIST(root='./data', 
-                           train=False, 
+test_dataset = dsets.MNIST(root='./data',
+                           train=False,
                            transform=transforms.ToTensor())
 
 '''
@@ -26,12 +26,12 @@ n_iters = 3000
 num_epochs = n_iters / (len(train_dataset) / batch_size)
 num_epochs = int(num_epochs)
 
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                           batch_size=batch_size, 
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
                                            shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
-                                          batch_size=batch_size, 
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
                                           shuffle=False)
 
 '''
@@ -41,39 +41,39 @@ class FeedforwardNeuralNetModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(FeedforwardNeuralNetModel, self).__init__()
         # Linear function 1: 784 --> 100
-        self.fc1 = nn.Linear(input_dim, hidden_dim) 
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
         # Non-linearity 1
         self.relu1 = nn.ReLU()
-        
+
         # Linear function 2: 100 --> 100
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
         # Non-linearity 2
         self.relu2 = nn.ReLU()
-        
+
         # Linear function 3: 100 --> 100
         self.fc3 = nn.Linear(hidden_dim, hidden_dim)
         # Non-linearity 3
         self.relu3 = nn.ReLU()
-        
+
         # Linear function 4 (readout): 100 --> 10
-        self.fc4 = nn.Linear(hidden_dim, output_dim)  
-    
+        self.fc4 = nn.Linear(hidden_dim, output_dim)
+
     def forward(self, x):
         # Linear function 1
         out = self.fc1(x)
         # Non-linearity 1
         out = self.relu1(out)
-        
+
         # Linear function 2
         out = self.fc2(out)
         # Non-linearity 2
         out = self.relu2(out)
-        
+
         # Linear function 2
         out = self.fc3(out)
         # Non-linearity 2
         out = self.relu3(out)
-        
+
         # Linear function 4 (readout)
         out = self.fc4(out)
         return out
@@ -112,7 +112,7 @@ STEP 7: TRAIN THE MODEL
 iter = 0
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
-        
+
         #######################
         #  USE GPU FOR MODEL  #
         #######################
@@ -122,26 +122,26 @@ for epoch in range(num_epochs):
         else:
             images = Variable(images.view(-1, 28*28))
             labels = Variable(labels)
-        
+
         # Clear gradients w.r.t. parameters
         optimizer.zero_grad()
-        
+
         # Forward pass to get output/logits
         outputs = model(images)
-        
+
         # Calculate Loss: softmax --> cross entropy loss
         loss = criterion(outputs, labels)
-        
+
         # Getting gradients w.r.t. parameters
         loss.backward()
-        
+
         # Updating parameters
         optimizer.step()
-        
+
         iter += 1
-        
+
         if iter % 500 == 0:
-            # Calculate Accuracy         
+            # Calculate Accuracy
             correct = 0
             total = 0
             # Iterate through test dataset
@@ -149,24 +149,26 @@ for epoch in range(num_epochs):
                 #######################
                 #  USE GPU FOR MODEL  #
                 #######################
-                images = Variable(images.view(-1, 28*28).cuda())
-                
+                if torch.cuda.is_available():
+                    images = Variable(images.view(-1, 28*28).cuda())
+                else:
+                    images = Variable(images.view(-1, 28*28))
                 # Forward pass only to get logits/output
                 outputs = model(images)
-                
+
                 # Get predictions from the maximum value
                 _, predicted = torch.max(outputs.data, 1)
-                
+
                 # Total number of labels
                 total += labels.size(0)
-                
+
                 #######################
                 #  USE GPU FOR MODEL  #
                 #######################
                 # Total correct predictions
                 correct += (predicted.cpu() == labels.cpu()).sum()
-            
+
             accuracy = 100 * correct / total
-            
+
             # Print Loss
             print('Iteration: {}. Loss: {}. Accuracy: {}'.format(iter, loss.data[0], accuracy))
